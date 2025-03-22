41dbecc3563733d0a1f28aa7c2cc7d793987ea40
unknown <yogeshkushwah775@gmail.com>
2025-03-22 22:39:59 +0530

    [Yogesh Kushwah] Add the project User rating to user Using the Registry-Configration-ApiGateway-CircuitBreaker-RateLimiter-Retry

--- UserService/UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.lcwd.user.service.controller;
 
 import com.lcwd.user.service.model.User;
+import com.lcwd.user.service.payload.ApiResponse;
 import com.lcwd.user.service.services.UserService;
+import com.lcwd.user.service.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +24,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+//    @Autowired
+//    private User user;
+
+
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     //Create the user
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
@@ -23,12 +38,39 @@ public class UserController {
         return new ResponseEntity<>(user1,HttpStatus.CREATED);
     }
 
+
+
     //get single user
     @GetMapping("/{userId}")
+//    @Retry(name="ratingRetry",fallbackMethod = "ratingHotelFall")
+// @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+
+    @RateLimiter(name="userRateLimiter",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+//        logger.info("Retry count: {}",retryCount);
+//        retryCount++;
         User user = userService.getUser(userId);
         return new ResponseEntity<>(user,HttpStatus.OK);
     }
+
+
+    //creating fallback method for circuitbreaker
+     public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+      logger.info("fall back executed when the some services are down");
+
+      User user=User.builder()
+              .email("Dummy@gmail")
+              .name("dummy")
+              .about("This is created dummy because some service is down")
+            .userId("1234").build();
+
+
+         return new ResponseEntity<>(user,HttpStatus.OK);
+
+
+     }
+
+
 
     //get all the users
     @GetMapping
--- UserService/UserService/src/main/java/com/lcwd/user/service/model/User.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/model/User.java
@@ -2,6 +2,7 @@ package com.lcwd.user.service.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User {
 
     @Id
